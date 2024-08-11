ed03f8478bbca157ccdfc2513aaca011a4e862fa
morjun <junmo2011@gmail.com>
2024-08-11 22:25:24 +0900

    Remove Download Manager support

--- user-overrides.js
+++ user-overrides.js
@@ -4,13 +4,13 @@
 user_pref("signon.rememberSignons", false);
 
 // For Download Master Compatibility
-user_pref("browser.download.start_downloads_in_tmp_dir", false); // [FF102+]
+//user_pref("browser.download.start_downloads_in_tmp_dir", false); // [FF102+]
 
 /** DOWNLOADS ***/
 /* 2651: enable user interaction for security by always asking where to download
  * [SETUP-CHROME] On Android this blocks longtapping and saving images
  * [SETTING] General>Downloads>Always ask you where to save files ***/
-user_pref("browser.download.useDownloadDir", true); // DEFAULT: false
+//user_pref("browser.download.useDownloadDir", true); // DEFAULT: false
 
 /* 2652: disable downloads panel opening on every download [FF96+] ***/
 //user_pref("browser.download.alwaysOpenPanel", true); // DEFAULT: false
@@ -19,7 +19,7 @@ user_pref("browser.download.useDownloadDir", true); // DEFAULT: false
 
 /* 2654: enable user interaction for security by always asking how to handle new mimetypes [FF101+]
  * [SETTING] General>Files and Applications>What should Firefox do with other files ***/
-user_pref("browser.download.always_ask_before_handling_new_types", false); // DEFAULT: true
+//user_pref("browser.download.always_ask_before_handling_new_types", false); // DEFAULT: true
 
 // user_pref("extensions.enabledScopes", 31) // [DEFAULT: 5]
 //user_pref("extensions.autoDisableScopes", 15); // [DEFAULT: 15]
@@ -32,4 +32,5 @@ user_pref("browser.download.always_ask_before_handling_new_types", false); // DE
  * could have been revoked) and/or you could be under attack (e.g. malicious blocking of OCSP servers)
  * [1] https://blog.mozilla.org/security/2013/07/29/ocsp-stapling-in-firefox/
  * [2] https://www.imperialviolet.org/2014/04/19/revchecking.html ***/
-user_pref("security.OCSP.require", false); // DEFAULT: true
+// user_pref("security.OCSP.require", false); // DEFAULT: true
+user_pref("browser.display.use_system_colors", true); // [DEFAULT: false NON-WINDOWS]
