4566f8c23319ab9efd42ad9a6c7e8e4775851bb2
morjun <anbndn5@gmail.com>
2025-01-01 20:54:01 +0900

    Add DoH settings

--- user-overrides.js
+++ user-overrides.js
@@ -35,3 +35,19 @@ user_pref("signon.rememberSignons", false);
 
 user_pref("security.OCSP.require", false); // DEFAULT: true
 user_pref("browser.display.use_system_colors", true); // [DEFAULT: false NON-WINDOWS]
+
+/* 0710: enable DNS-over-HTTPS (DoH) [FF60+]
+ * 0=default, 2=increased (TRR (Trusted Recursive Resolver) first), 3=max (TRR only), 5=off (no rollout)
+ * see "doh-rollout.home-region": USA 2019, Canada 2021, Russia/Ukraine 2022 [3]
+ * [SETTING] Privacy & Security>DNS over HTTPS
+ * [1] https://hacks.mozilla.org/2018/05/a-cartoon-intro-to-dns-over-https/
+ * [2] https://wiki.mozilla.org/Security/DOH-resolver-policy
+ * [3] https://support.mozilla.org/kb/firefox-dns-over-https
+ * [4] https://www.eff.org/deeplinks/2020/12/dns-doh-and-odoh-oh-my-year-review-2020 ***/
+   user_pref("network.trr.mode", 3);
+/* 0712: set DoH provider
+ * The custom uri is the value shown when you "Choose provider>Custom>"
+ * [NOTE] If you USE custom then "network.trr.uri" should be set the same
+ * [SETTING] Privacy & Security>DNS over HTTPS>Increased/Max>Choose provider ***/
+   user_pref("network.trr.uri", "https://adguard.junserver.win/dns-query");
+   user_pref("network.trr.custom_uri", "https://adguard.junserver.win/dns-query");
